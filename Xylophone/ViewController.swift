69bab224bfb691fafc4b3b754db0737a08fc1b21
smease12 <scmease93@gmail.com>
2023-12-26 18:34:17 -0500

    Add AVAudioPlayer var

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
