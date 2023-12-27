734babd353a0cc5f5ed234a685c6fe1734f144ae
smease12 <scmease93@gmail.com>
2023-12-26 19:11:44 -0500

    Simplify code to play sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
     
-    var player: AVAudioPlayer?
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,26 +13,10 @@ class ViewController: UIViewController {
         playSound()
     }
     
-    func playSound(){
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav" ) else { return }
-        
-        do{
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            /*For iOS 11*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            
-            guard let player = player else { return }
-            
-            player.play()
-        }
-        catch let error{
-            print(error.localizedDescription)
-        }
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
     }
-    
-    
-
 }
-
